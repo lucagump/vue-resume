4c5d03717b2b5c18ead8389222f1e7c4850e3a04
Luca Martinelli <l.martinelli@hotmail.com>
2020-04-19 01:07:32 +0200

    bo

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div class="main-wrapper">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
       <a class="navbar-brand js-scroll-trigger" href="#about">
-        <span class="d-block d-lg-none">Luca Martinelli</span>
         <span class="d-none d-lg-block">
-          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="./assets/profile.jpg" alt="">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="./assets/profile.jpg">
         </span>
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,10 +32,9 @@
         </ul>
       </div>
     </nav>
-
     <div class="container-fluid p-0">
-
       <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="about-bg"></div>
         <div class="my-auto">
           <h1 class="mb-0">{{ name }}
             <span class="text-primary">{{ surname }}</span>
@@ -44,10 +42,11 @@
           <div class="subheading mb-5">{{ address }}
             <a href="mailto:name@email.com">{{ email }}</a>
           </div>
-          <p class="mb-5">{{ aboutMe }}</p>
+          <p class="mb-0">{{ aboutMe1 }}</p>
+          <p class="mb-5">{{ aboutMe2 }}</p>
           <ul class="list-inline list-social-icons mb-0">
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https://www.facebook.com/luca.martinelli.12139">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
                   <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
@@ -55,15 +54,15 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https://www.instagram.com/lucagump">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
-                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                  <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
                 </span>
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/luca-martinelli-7ab713168">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
                   <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
@@ -71,7 +70,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https:\\www.github.com/lucagump">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
                   <i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -81,11 +80,9 @@
           </ul>
         </div>
       </section>
-
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
         <div class="my-auto">
           <h2 class="mb-5">Experience</h2>
-
           <div class="resume-item d-flex flex-column flex-md-row mb-5" v-for="job in experience" :key="job.description">
             <div class="resume-content mr-auto">
               <h3 class="mb-0">{{ job.position }}</h3>
@@ -96,119 +93,82 @@
               <span class="text-primary">{{ job.startDate }} - {{ job.endDate }}</span>
             </div>
           </div>
-
         </div>
-
       </section>
-
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
         <div class="my-auto">
           <h2 class="mb-5">Education</h2>
-
-          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+          <div class="resume-item d-flex flex-column flex-md-row mb-5" v-for="study in education" :key="study.description">
             <div class="resume-content mr-auto">
-              <h3 class="mb-0">University of Colorado Boulder</h3>
-              <div class="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 3.23</p>
+              <h3 class="mb-0">{{study.course}}</h3>
+              <div class="subheading mb-3">{{study.university}}</div>
+              <div>{{study.description}}</div>
             </div>
             <div class="resume-date text-md-right">
-              <span class="text-primary">August 2006 - May 2010</span>
+              <span class="text-primary">{{ study.startDate }} - {{ study.endDate }}</span>
             </div>
           </div>
-
-          <div class="resume-item d-flex flex-column flex-md-row">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">James Buchanan High School</h3>
-              <div class="subheading mb-3">Technology Magnet Program</div>
-              <p>GPA: 3.56</p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">August 2002 - May 2006</span>
-            </div>
-          </div>
-
         </div>
       </section>
-
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
         <div class="my-auto">
           <h2 class="mb-5">Skills</h2>
 
-          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <div class="subheading mb-3"> As a Developer</div>
+          <ul class="fa-ul mb-3" v-for="skill in skills" :key="skill.sk">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              {{skill.sk}}</li>
+          </ul>
+          <div class="subheading mb-3"> As a Human</div>
+          <ul class="fa-ul mb-3" v-for="softskill in softskills" :key="softskill.ss">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              {{softskill.ss}}</li>
+          </ul>
+          <div class="subheading mb-4">Programming Languages &amp; Tools</div>
           <ul class="list-inline list-icons">
             <li class="list-inline-item">
               <i class="devicons devicons-html5"></i>
             </li>
             <li class="list-inline-item">
-              <i class="devicons devicons-css3"></i>
+              <i class="devicons devicons-github"></i>
             </li>
             <li class="list-inline-item">
               <i class="devicons devicons-javascript"></i>
             </li>
             <li class="list-inline-item">
-              <i class="devicons devicons-jquery"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-sass"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-less"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-bootstrap"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-wordpress"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-grunt"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-gulp"></i>
+              <i class="devicons devicons-mongodb"></i>
             </li>
             <li class="list-inline-item">
               <i class="devicons devicons-npm"></i>
             </li>
-          </ul>
-
-          <div class="subheading mb-3">Workflow</div>
-          <ul class="fa-ul mb-0">
+            <li class="list-inline-item">
+              <i class="devicons devicons-nodejs"></i>
+            </li>
             <li>
-              <i class="fa-li fa fa-check"></i>
-              Mobile-First, Responsive Design</li>
-            <li>
-              <i class="fa-li fa fa-check"></i>
-              Cross Browser Testing &amp; Debugging</li>
-            <li>
-              <i class="fa-li fa fa-check"></i>
-              Cross Functional Teams</li>
-            <li>
-              <i class="fa-li fa fa-check"></i>
-              Agile Development &amp; Scrum</li>
+            <i class="fa fa-google-drive"></i>
+            </li>
           </ul>
         </div>
       </section>
-
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
         <div class="my-auto">
           <h2 class="mb-5">Interests</h2>
-          <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skiier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-          <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development world.</p>
+          <p>{{interests.first_description}}</p>
+          <p class="mb-0">{{interests.second_description}}</p>
         </div>
       </section>
-
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
-        <div class="my-auto" v-for="award in awards" :key="award.description">
-          <h2 class="mb-5">Awards &amp; Certifications</h2>
+        <div class="my-auto">
+          <h2 class="mb-5">Certifications</h2>
           <ul class="fa-ul mb-0">
             <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              {{award.descriprion}}</li>
+            <i class="fa-li fa fa-check"></i>
+              {{awards.certificate}}</li>
           </ul>
         </div>
       </section>
-
     </div>
   </div>
 </template>
@@ -220,9 +180,10 @@ export default {
       title: 'Luca Martinelli',
       name: 'Luca',
       surname: 'Martinelli',
-      address: 'Strada Boiane 3 · Suzzara (MN), IT 46029 · (+39) 333-4895818 ·',
+      address: 'Strada Boiane 3 · Suzzara (MN) IT 46029 · (+39) 333-4895818 ·',
       email: 'l.martinelli@hotmail.com',
-      aboutMe: 'I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.',
+      aboutMe1: 'I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews.',
+      aboutMe2: 'Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.',
       experience: [
         {
           position: 'Software Engineer',
@@ -234,52 +195,57 @@ export default {
         {
           position: 'Project Manager and Software Developer',
           company: 'E-Agle Trento Racing Team',
-          description: 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.',
+          description: 'Design and development of the user-interface for Chimera Evoluzione\'s Steering Wheel (Qt). During one of the competition in Varano (PR) 2018 - "Best HMI Solution" Prize from Automobili Lamborghini. Design and Development of the Telemetry Solution using MQTT protocol for a Cloud Based Web Application using Raspberry PI, MongoDB and Vue. Project-Management related to the development of HW and SW solution for "Telemetria" and"Fenice Steering Wheel"',
           startDate: 'September 2017',
           endDate: 'Present'
         },
         {
-          position: 'Junior Web Designer',
-          company: 'Shout! Media Productions',
-          description: 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.',
-          startDate: 'July 2010',
-          endDate: 'December 2011'
+          position: 'Full Stack Developer',
+          company: 'Prom Facility',
+          description: 'Design and development of micro-services web application used to collect and display data from machinery and other internal purposes.',
+          startDate: 'Novembre 2018',
+          endDate: 'April 2019'
         }
       ],
       education: [
         {
-          course: 'ICT Innovation Software and Service Architectures',
+          course: 'Master Degree in ICT Innovation Software and Service Architectures',
           university: 'Università degli Studi di Trento',
-          description: 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.',
+          description: '',
           startDate: 'September 2019',
           endDate: 'Present'
         },
         {
           course: 'Bachelor Degree in Computer Science',
           university: 'Università degli Studi di Trento',
-          description: 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.',
+          description: '',
           startDate: 'September 2015',
           endDate: 'July 2019'
         }
       ],
       interests: [
         {
-          first_descriprion: 'Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skiier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking',
-          second_description: 'When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development world.'
+          first_description: 'Apart from being a student and a developer, I enjoy most of my time being a strict contact with nature. In the winter, I am an Snowboarder. I enjoy mountain biking',
+          second_description: 'When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring problem-solver, and I spend a large amount of my time exploring the latest technolgy in the software development and design interaction.'
         }
       ],
-      awards: [
-        {
-          descriprion: 'Best HMI Solution - Automobili Lamborghini S.p.A.'
-        }
-      ],
+      awards: {
+        description: 'Best HMI Solution - Automobili Lamborghini S.p.A.',
+        certificate: 'English: B2 certified by "Centro Linguistico di Ateneo - Università degli Studi di Trento"'
+      },
       skills: [
-        'English: B2 certified by "Centro Linguistico di Ateneo - Università degli Studi di Trento"',
-        'Qt, C++, QML: Cross-Platform Development',
-        'Can-Bus: Linux Embedded Systems',
-        'Linux: Embedded Systems and Server',
-        'Node.JS & Vue.JS: Services and Web Application',
-        'C# and .NET: First Steps'
+        {sk: 'Qt, C++, QML: Cross-Platform Development'},
+        {sk: 'Can-Bus: Linux Embedded Systems'},
+        {sk: 'Linux: Embedded Systems and Server'},
+        {sk: 'Node.JS & Vue.JS: Services and Web Application'},
+        {sk: 'C# and .NET: First Steps'}
+      ],
+      softskills: [
+        {ss: 'Design Thinking'},
+        {ss: 'Critical Thinking'},
+        {ss: 'Emotional Intelligence'},
+        {ss: 'Co-Design'},
+        {ss: 'Empathy'}
       ],
       field: 'Automotive, Formula Student, Linux, Snowboard, Athletics and Mountain Bike'
     }
@@ -399,9 +365,9 @@ h2 {
     padding: 0.5rem;
   }
   #sideNav .navbar-brand .img-profile {
-    max-width: 10rem;
-    max-height: 10rem;
-    border: 0.5rem solid rgba(255, 255, 255, 0.2);
+    max-width: 13rem;
+    max-height: 13rem;
+    border: 0.1rem solid rgba(255, 255, 255, 0.7);
   }
   #sideNav .navbar-collapse {
     display: flex;

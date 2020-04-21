0e1d1026240f6d223b4081a0df0506c95e51048e
Luca Martinelli <l.martinelli@hotmail.com>
2020-04-21 17:12:50 +0200

    codice per scordarsi degli altri

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-wrapper">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" id="sideNav">
       <a class="navbar-brand js-scroll-trigger" href="#about">
         <span class="d-none d-lg-block">
           <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="./assets/profile.jpg">

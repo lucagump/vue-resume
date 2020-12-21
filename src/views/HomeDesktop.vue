87169fba6fd56301ff15445da5b78245d6fc8f25
Luca Martinelli <l.martinelli@hotmail.com>
2020-12-21 18:36:01 +0100

    resolution photo changed

--- src/views/HomeDesktop.vue
+++ src/views/HomeDesktop.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
       <a class="navbar-brand" href="#about">
         <span class="d-none d-lg-block">
-          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="../assets/profile.jpg">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="../assets/Profilo-min.jpg">
         </span>
       </a>
       <button class="navbar-toggler" type="button" style="border: 0" v-on:click="changeText()">
